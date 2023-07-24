e004760baa2e4d146097047b36f2966b25a0bafb
Dmitry Shakirov <117525799+DShakirov@users.noreply.github.com>
2023-07-24 21:50:21 +0300

    Update celery.py

--- imperiasevastopol_bot/imperiasevastopol_bot/celery.py
+++ imperiasevastopol_bot/imperiasevastopol_bot/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dtb.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'imperiasevastopol_bot.settings'
 
 app = Celery('imperiasevastopol_bot')
 
